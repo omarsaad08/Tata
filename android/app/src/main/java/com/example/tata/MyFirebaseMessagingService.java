c3d360ca80a75babcfe3ac15bd70fec6cbeda60c
omar <omarsaad08@outlook.com>
2025-03-09 03:11:42 +0200

    should be ready to deploy

--- android/app/src/main/java/com/example/tata/MyFirebaseMessagingService.java
+++ android/app/src/main/java/com/example/tata/MyFirebaseMessagingService.java
@@ -8,7 +8,9 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
